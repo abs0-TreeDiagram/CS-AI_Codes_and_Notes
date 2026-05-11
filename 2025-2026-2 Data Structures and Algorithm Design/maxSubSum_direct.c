c9e9b50be45fbbabbeeff32f3c0aca5c61ebc7b9
abs0-TreeDiagram <jfjf448@163.com>
2026-05-11 19:13:10 +0800

    Add files via upload

---
+++ 2025-2026-2 Data Structures and Algorithm Design/maxSubSum_direct.c
@@ -0,0 +1,30 @@
+/*最大子段和问题：
+找出给定数列的连续子列
+使得该子列的和在所有这样的连续子列中和最大
+输出最大的求和结果
+直接法：循环遍历子段*/
+#include<stdio.h>
+
+#define N 9
+
+int l[N]={2,-3,1,5,-1,3,-2,-3,3};
+
+int main()
+{
+    int max=0,sum,start,end;
+    for (int i=0;i<N;i++)//遍历每个连续子段
+    {
+        sum=0;
+        for (int j=i;j<N;j++)
+        {
+            sum+=l[j];
+            if (sum>max)
+            {
+                max=sum;
+                start=i;
+                end=j;
+            }
+        }
+    }
+    printf("Max sum:%d\nStart:%d\nEnd:%d",max,start,end);
+}
